8df4abcb80da15d7aeabf10f0b507b267fc2bacb
Antoine Frénoy <afrenoy@gmx.com>
2016-12-21 14:13:14 +0100

    Input files and initial version of the code to parse the html input chapter and produce a clean webpage

---
+++ Makefile
@@ -0,0 +1,27 @@
+all: larossa.html
+
+larossa.html: references-4.csv table1-4.csv table2-4.csv
+	./generatewebpage.py table1-4.csv table2-4.csv references-4.csv > larossa.html
+
+clean:
+	rm -f table1-2.csv table1-3.csv table2-2.html table2-3.csv references-1.html references-2.html references-3.html references-4.csv table1-4.csv table2-4.csv
+
+table1-4.csv: table1-1.csv
+	cat table1-1.csv |sed 's/<i>\([^<]*\)<\/i>/*\1*/g' |sed 's/<b>\([^<]*\)<\/b>/**\1**/g' > table1-2.csv
+	cat table1-2.csv |sed 's/;\*\([a-zA-Z]*\)\*$$/;\1/g' > table1-3.csv
+	./createtable1.py table1-3.csv > table1-4.csv
+
+table2-4.csv: table2-1.html
+	cat table2-1.html |sed 's/<br\/>//g' > table2-2.html
+	cat table2-2.html |tr '\n' ';' |sed 's/;<i>\([a-z][a-z][a-z]\)/{<i>\1/g' |tr '{' '\n' > table2-3.csv
+	echo "<strong>TABLE 2</strong> Genes for which selections exist in <i>E. coli</i> and <i>S. typhimurium</i>" > table2-4.csv
+	echo "Gene;Organism;Selection;References;Alteration" >> table2-4.csv
+	cat table2-3.csv |sed 's/^<i>\([a-zA-Z]*\)<\/i>/\1/g' |sed 's/\([;\|,]\)[^;,]*Epelbaum[^;,]*\([;\|,]\)/\1872\2/g' |sed 's/\([;\|,]\)[^;,]*Comai[^;,]*\([;\|,]\)/\1869\2/g' |sed 's/\([;\|,]\)[^;,]*Lin et al[^;,]*\([;\|,]\)/\1870\2/g' |sed 's/\([;\|,]\)[^;,]*Miller[^;,]*\([;\|,]\)/\1871\2/g' |sed 's/\([;\|,]\)[^;,]*Freitag[^;,]*\([;\|,]\)/\1873\2/g' |sed 's/–/-/g' >> table2-4.csv
+
+references-4.csv: references.html supref.txt
+	cat references.html|gsed 's/^\(\&\#160;[ ]*\)\+//g' |sed 's/\&\#160;/ /g' > references-1.html
+	cat references-1.html |tr '\n' ';' |gsed 's/<br\/>;\([0-9]\{1,3\}[a]\?\.\)/}\1/g' |tr '}' '\n' > references-2.html
+	cat references-2.html |sed 's/;//g' |sed 's/<br\/>/ /g' |sed 's/$$/<br\/>/g' > references-3.html
+	cat references-3.html|sed 's/<\/b> *<b>/ /g' |gsed 's/^\([0-9]*[a]\?\)\. *<b>\([a-zA-Z ,\.’èöé-]*\)\.<\/b> *\([0-9]*\)\(.*\)<i>\([a-zA-Z \.:,é-]*\)<\/i>\( *<b>\|[^<]*<br\)/\1;\2;\3;\4;\5\6/g' |sed 's/<br\/>//g' > references-4.csv
+	cat supref.txt >> references-4.csv
+
---
+++ generatewebpage.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import markdown
+def translate(string):
+    return markdown.markdown(string).replace('<p>','').replace('</p>','')
+
+import sys
+if len(sys.argv)<4:
+    print("3 arguments expected: 1st table, 2nd table, and references")
+    exit(-1)
+
+print('<!DOCTYPE html>\n<html>\n<head>\n\t<meta charset=\"UTF-8\">\n\t<title>Title of the document</title>\n</head>\n<body>\n')
+
+# Table 1
+def printtable1(ftab1):
+    f=open(ftab1,'r')
+    tab=[translate(t) for t in f.readlines()]
+    f.close()
+
+    print('<h1>%s</h1>'%tab[0])
+    print('<table>')
+    for line in tab[1:]:
+        print('<tr>')
+        tokens=line.split(';')
+        for t in tokens:
+            print('<td>')
+            print(t)
+            print('</td>')
+        print('</tr>')
+    print('</table>')
+
+printtable1(sys.argv[1])
+
+# Table 2
+def printtable2(ftab2):
+    g=open(ftab2,'r')
+    tab=g.readlines()
+    g.close()
+
+    print('<h1>%s</h1>'%tab[0])
+    print('<table>')
+
+    tokens=tab[1].split(';')
+    print('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>'%(tokens[0],tokens[1],tokens[2],tokens[3],tokens[4]),end='')
+
+    for line in tab[2:]:
+        if line.count(';')==3:
+            #print(line)
+            line=line.rstrip('\n')+'; \n'
+            #print(line)
+        print('<tr>')
+        tokens=line.split(';')
+        print('<td>%s</td><td>%s</td><td>%s</td>'%(tokens[0],tokens[1],tokens[2]),end='')
+        allrefs=[]
+        for ur in tokens[3].split(','):
+            ur=ur.strip(' ')
+            if '-' in ur:
+                dual=ur.split('-')
+                for i in range(int(dual[0]),int(dual[1])+1):
+                    allrefs.append(str(i))
+            else:
+                allrefs.append(ur)
+        print('<td>',end='')
+        for ref in allrefs:
+            if '%0A' in ref:
+                print('Hem hem')
+                exit(0)
+            print('<a href="#ref%s">%s</a> '%(ref,ref),end='')
+        print('</td><td>%s</td>'%tokens[4],end='')
+        print('</tr>')
+    print('</table>')
+
+printtable2(sys.argv[2])
+
+# References
+def printtableref(fref):
+    h=open(fref,'r')
+    refs=h.readlines()
+    h.close()
+
+    print('<h1>References</h1>')
+    print('<table>')
+    references=dict()
+    for ref in refs:
+        tokens=ref.split(';')
+        if ' p. ' in tokens[3]:
+            tokens[3]=tokens[3].split(' p. ')[0].rstrip(',')+'.'
+        tokens[3]=tokens[3].lstrip(' .')
+        references[tokens[0]]=(tokens[1],int(tokens[2]),tokens[3])
+        strquery=tokens[3].rstrip(' .').replace('<i>','').replace('</i>','')
+        print('<tr><td><a name="ref%s"></a>%s</td><td>%s <i>%s</i>. %s</td><td><a href="https://scholar.google.ch/scholar?hl=en&q=%s">google scholar</a></td></tr>'%(tokens[0],tokens[0],tokens[1],tokens[2],tokens[3],strquery))
+    print('</table>')
+
+printtableref(sys.argv[3])
+
+print('</body>\n</html>')
