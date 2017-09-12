0bf7cf17adc64e20dcd611e41a708daf7dbea1c3
Antoine Frénoy <afrenoy@gmx.com>
2017-09-12 18:05:17 +0200

    small modifications of the webpage to generate valid html

--- generatewebpage.py
+++ generatewebpage.py
@@ -18,7 +18,7 @@ def printtable1(ftab1):
     tab=[translate(t) for t in f.readlines()]
     f.close()
 
-    print('<h1><a name="tab1"></a>%s</h1>'%tab[0],file=output)
+    print('<h1><a id="tab1"></a>%s</h1>'%tab[0],file=output)
     print('<table>',file=output)
     for line in tab[1:]:
         print('<tr>',file=output)
@@ -38,7 +38,7 @@ def printtable2(ftab2):
     tab=g.readlines()
     g.close()
 
-    print('<h1><a name="tab2"></a>%s</h1>'%tab[0],file=output)
+    print('<h1><a id="tab2"></a>%s</h1>'%tab[0],file=output)
     print('<table>',file=output)
 
     tokens=tab[1].split(';')
@@ -80,7 +80,7 @@ def printtableref(fref):
     refs=h.readlines()
     h.close()
 
-    print('<h1><a name="ref"></a>References</h1>',file=output)
+    print('<h1><a id="ref"></a>References</h1>',file=output)
     print('<table>',file=output)
     references=dict()
     for ref in refs:
@@ -90,16 +90,16 @@ def printtableref(fref):
         tokens[3]=tokens[3].lstrip(' .')
         references[tokens[0]]=(tokens[1],int(tokens[2]),tokens[3])
         strquery=tokens[3].rstrip(' .').replace('<i>','').replace('</i>','')
-        gscholar='https://scholar.google.ch/scholar?hl=en&q=%s'%strquery
-        print('<tr><td><a name="ref%s"></a>%s</td><td>%s <i>%s</i>. %s</td><td><a href="%s">google scholar</a></td></tr>'%(tokens[0],tokens[0],tokens[1],tokens[2],tokens[3],gscholar),file=output)
+        gscholar='https://scholar.google.ch/scholar?hl=en&q=%s'%(strquery.replace(' ','%20'))
+        print('<tr><td><a id="ref%s"></a>%s</td><td>%s <i>%s</i>. %s</td><td><a href="%s">google scholar</a></td></tr>'%(tokens[0],tokens[0],tokens[1],tokens[2],tokens[3],gscholar),file=output)
         #refs.getsource(gscholar,tokens[0]) # Try to download the pdf / find doi / bibtex record / ...
     print('</table>',file=output)
     return(output.getvalue())
 
 output=io.StringIO()
-print('<!DOCTYPE html>\n<html>\n<head>\n\t<meta charset=\"UTF-8\">\n\t<title>Larossa</title>\n\t<link rel="stylesheet" href="style.css">\n</head>\n<body>\n',file=output)
+print('<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset=\"UTF-8\">\n\t<title>Larossa</title>\n\t<link rel="stylesheet" href="style.css">\n</head>\n<body>\n',file=output)
 print('<div id="menu"><nav><ul><li><a href="#about">About</a></li><li><a href="#tab1">Selections giving rise to mutants</a></li><li><a href="#tab2">Genes for which selections exist</a></li><li><a href="#ref">References</a></li></ul></nav></div>\n<div id="main">',file=output)
-print('<h1 id="about">About this document</h1><p>This is data from chapter 139 of E coli and Salmonella, reproduced without permission. </br>The pdf has been converted to html and parsed using a few bash and python scripts availabe on <a href="http://github.com/frenoy/ecosal">github</a>.</p><p>The raw data can be downloaded as csv files:</p><ul><li><a href="table1-4.csv">Table 1</a>: Selections giving rise to mutants</li><li><a href="table2-4.csv">Table 2</a>: Genes for which selections exist</li><li><a href="references-4.csv">Table 3</a>: References</li></ul>',file=output)
+print('<h1 id="about">About this document</h1><p>This is data from chapter 139 of E coli and Salmonella, reproduced without permission. <br>The pdf has been converted to html and parsed using a few bash and python scripts availabe on <a href="http://github.com/frenoy/ecosal">github</a>.</p><p>The raw data can be downloaded as csv files:</p><ul><li><a href="table1-4.csv">Table 1</a>: Selections giving rise to mutants</li><li><a href="table2-4.csv">Table 2</a>: Genes for which selections exist</li><li><a href="references-4.csv">Table 3</a>: References</li></ul>',file=output)
 print(printtable1(sys.argv[1]),file=output)
 print(printtable2(sys.argv[2]),file=output)
 print(printtableref(sys.argv[3]),file=output)
